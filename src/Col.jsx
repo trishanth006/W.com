b9a650f3c5dd91de36809bd12508fc56963180bf
trishanth006 <131153863+trishanth006@users.noreply.github.com>
2024-09-23 23:17:10 +0530

    Update Col.jsx

--- src/Col.jsx
+++ src/Col.jsx
@@ -1,17 +1,25 @@
 import furniture from "./assets/furniture.webp"
+import CustImg from "./CustImg"
 function Col(){
     return(
         
         <div class="card">
-        <div class="card-image">
-          <img src={furniture} alt="Card Image"></img>
-        </div>
-        <div class="card-image1">
-          <img src={furniture} alt="Card Image"></img>
-        </div>
-        <div class="card-image2">
-          <img src={furniture} alt="Card Image"></img>
-        </div>
+          <div className="col about">
+            <h1 className="about-title">What Are We About</h1>
+            <p className="about-para">Lorem ipsum dolor sit amet consectetur adipisicing elit.  Ullam quasi culpa perspiciatis sit, voluptates officiis.Ullam quasi culpa perspiciatis sit, voluptates officiis.</p>
+            <button className="about-btn">EXPLORE MORE</button>
+          </div>
+          <div className="col gallery">
+              <img src={furniture} alt="" className="custimg" />
+              <img src={furniture} alt="" className="custimg"/>
+              <img src={furniture} alt="" className="custimg"/>
+              <img src={furniture} alt="" className="custimg"/>
+              <img src={furniture} alt="" className="custimg"/>
+              <img src={furniture} alt="" className="custimg"/>
+              <img src={furniture} alt="" className="custimg"/>
+              <img src={furniture} alt="" className="custimg"/>
+              <img src={furniture} alt="" className="custimg"/>
+          </div>
       </div>
     )
 }
